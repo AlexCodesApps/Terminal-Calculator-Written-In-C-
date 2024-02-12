390b98d3f9c61a3c40c6b02b9b7eea516a86701b
AlexCodesApps <141887614+AlexCodesApps@users.noreply.github.com>
2024-02-12 17:03:04 -0500

    Update main.c
    Added Power and Operator Invalid

--- main.c
+++ main.c
@@ -7,12 +7,14 @@ bool;
 #endif
 
 typedef enum Operator
-{Add, Subtract, Multiply, Divide} operator;
+{Add, Subtract, Multiply, Divide, Power} operator;
 
 void CalcInit();
+void CalcInit2(double fResults);
 double GetInput();
 int GetOperator();
 double Calculate();
+double Pow();
 
 double num1;
 double num2;
@@ -27,6 +29,7 @@ int main() {
         printf("\nTry again? (1 for yes, any other input for no.): ");
         getchar();
         if (scanf("%d", &continueCalc) != true) {
+            getchar();
             continueCalc = 0;
         }
     }
@@ -39,15 +42,30 @@ void CalcInit() {
     printf("\nInput #2: ");
     num2 = GetInput(num2);
     printf("\nInputs: %.8g, %.8g\n", num1, num2);
-    printf("Operator (Add = 0, Subtract = 1, Multiply = 2, Divide = 3): ");
+    printf("Operator (Add = 0, Subtract = 1, Multiply = 2, Divide = 3, Pow = 4): ");
     Operator = GetOperator(Operator);
     printf("Calculating...");
     result = Calculate();
     printf("\nThe result is: %.8g!",result);
 }
 
+void CalcInit2(double fResults) {
+    printf("\nWelcome To The Terminal Calculator!");
+    printf("\nInput #1: %lf", fResults);
+    num1 = fResults;
+    printf("\nInput #2: ");
+    num2 = GetInput(num2);
+    printf("\nInputs: %.8g, %.8g\n", num1, num2);
+    printf("Operator (Add = 0, Subtract = 1, Multiply = 2, Divide = 3, Pow = 4): ");
+    Operator = GetOperator(Operator);
+    printf("Calculating...");
+    result = Calculate();
+    printf("\nThe result is: %.8g!",result);
+
+}
+
 double GetInput(double num) {
-    if (scanf("%lf", &num) == false) {
+    if (scanf("%lf", &num) == (false)) {
         printf("Invalid input, try again.\n");
         getchar();
         GetInput(num);
@@ -56,9 +74,10 @@ double GetInput(double num) {
 }
 
 int GetOperator(int num) {
-    if (scanf("%d", &num) == false) {
+    bool valid = (scanf("%d", &num));
+    if (valid == false || num > 4 || num < 0) {
         printf("Invalid input, try again.\n");
-        getchar();
+        int num2;
         GetOperator(num);
     }
     return num;
@@ -69,5 +88,23 @@ double Calculate() {
     if (Operator == Subtract) {return num1 - num2;}
     if (Operator == Multiply) {return num1 * num2;}
     if (Operator == Divide) {return num1 / num2;}
+    if (Operator == Power) {return Pow(num1, num2);}
     return 0;
+}
+
+double Pow(double num, double pow) {
+    double result = num;
+    if (pow > 0) {
+        for (int i = 1; i < pow; i++)
+        {
+            result *= num;
+        }
+    }
+    else if (pow < 0)
+        for (int i = 1; i > pow; i--)
+        {
+            result *= 1/num;
+        }
+    else result = 1;
+    return result;
 }
